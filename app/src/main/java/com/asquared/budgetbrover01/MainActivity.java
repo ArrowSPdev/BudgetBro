980fbe58b5a7a1ee3befe4906bc1c90b32324cb8
ArrowSPdev <aaronbranagan@gmail.com>
2023-03-07 23:57:40 +0000

    hi cathal

--- app/src/main/java/com/asquared/budgetbrover01/MainActivity.java
+++ app/src/main/java/com/asquared/budgetbrover01/MainActivity.java
@@ -2,26 +2,11 @@ package com.asquared.budgetbrover01;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.asquared.budgetbrover01.databinding.ActivitySavingsInputBinding;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.asquared.budgetbrover01.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,20 +29,5 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        svgsBtntest= (Button) findViewById(R.id.svgsBtntest);
-        svgsBtntest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-
-            @Override
-            public void startSavingsInput(View view) {
-
-                Intent intent=new Intent(MainActivity.this,SavingsInput.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
